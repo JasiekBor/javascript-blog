7f9cc2a135a0bfbdf6ba95babe20cc17e57c6d0e
Jan Borowski <contact@jbdeveloper.eu>
2022-04-08 16:05:33 +0200

    remove contents of titlelist

--- js/script.js
+++ js/script.js
@@ -37,3 +37,30 @@ const targetArticle = document.querySelector(articleSelector);
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+const optArticleSelector = '.post',
+  optTitleselector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+    /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector);
+titleList.innerHTML = '';
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+
+
